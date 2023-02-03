d0730ccbbc2a245316f2f802fa24d64e044a54e2
kouzber123 <tom.nguyen@windowslive.com>
2023-02-03 12:35:39 +0200

    build: refactored pokemonService file

--- src/app/components/pokemon-catalogue/pokemon-catalogue.component.ts
+++ src/app/components/pokemon-catalogue/pokemon-catalogue.component.ts
@@ -1,52 +1,52 @@
 import { OnInit } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { Pokemon } from 'src/app/_models/pokemon.model'
-import { PokemonsService } from 'src/app/services/pokemons.service';
+import { Pokemon } from 'src/app/_models/pokemon.model';
+import { PokemonsService } from 'src/app/_services/pokemons.service';
 import { Component } from '@angular/core';
 import { DecimalPipe, NgFor } from '@angular/common';
 import { FormControl, FormsModule } from '@angular/forms';
-import { NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbPaginationModule,
+  NgbTypeaheadModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
-import { take, map, tap, startWith, filter } from 'rxjs/operators'
+import { take, map, tap, startWith, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon-catalogue',
   templateUrl: './pokemon-catalogue.component.html',
-  styleUrls: ['./pokemon-catalogue.component.css']
+  styleUrls: ['./pokemon-catalogue.component.css'],
 })
-
 export class PokemonCatalogueComponent implements OnInit {
   page = 1;
-  pageSize= 10;
-  collectionSize = 1279
-  showingPokemons: Pokemon[] = []
+  pageSize = 10;
+  collectionSize = 1279;
+  showingPokemons: Pokemon[] = [];
 
   constructor(private readonly pokemonsService: PokemonsService) {}
 
   ngOnInit(): void {
-    this.pokemonsService.fetchPokemons()
-    this.refresPage()
-    this.refreshPokemons()
+    this.pokemonsService.fetchPokemons();
+    this.refresPage();
+    this.refreshPokemons();
   }
 
-  public get pokemons$(): Observable<Pokemon[]> {  
-    return this.pokemonsService.showingPokemons$
+  public get pokemons$(): Observable<Pokemon[]> {
+    return this.pokemonsService.showingPokemons$;
   }
 
   private refresPage = () => {
-    const result = this.pokemonsService.pokemons$
-    .subscribe(pokemons => {
-      this.showingPokemons = pokemons.slice(((this.page-1)*this.pageSize),(this.page*this.pageSize))
-    })
-    result.unsubscribe()
-  }
+    const result = this.pokemonsService.pokemons$.subscribe((pokemons) => {
+      this.showingPokemons = pokemons.slice(
+        (this.page - 1) * this.pageSize,
+        this.page * this.pageSize
+      );
+    });
+    result.unsubscribe();
+  };
 
   refreshPokemons() {
-    this.refresPage()
-    this.pokemonsService.fetchPokemonPictures(this.showingPokemons)
+    this.refresPage();
+    this.pokemonsService.fetchPokemonPictures(this.showingPokemons);
   }
-  
 }
-
-
-
