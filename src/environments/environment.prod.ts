53382fddeeb4e674c74c55479cd4c151ad96783d
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-02-06 19:05:41 +0200

    removed environments folder from git

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "01234567890",
-  apiPokemon: "https://mixolydian-handy-brachiosaurus.glitch.me/trainers"
-}
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-  production: false,
-  apiKey: "1234567890",
-  apiPokemon: "https://mixolydian-handy-brachiosaurus.glitch.me/trainers"
-
-};
